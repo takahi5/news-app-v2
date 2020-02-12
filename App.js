59347cd0e16885b377f1a8429616d4d04d2d5de0
Takahiko Wada <wada.takahiko@gmail.com>
2020-02-12 14:08:42 +0900

    Added userState

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import {StyleSheet, View, FlatList, SafeAreaView} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, FlatList, SafeAreaView} from 'react-native';
 import ListItem from './components/ListItem';
-import articles from './dummies/articles';
+import dummyArticles from './dummies/articles';
 
 const styles = StyleSheet.create({
   container: {
@@ -11,6 +11,8 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
+  const [articles, setArticles] = useState(dummyArticles);
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
