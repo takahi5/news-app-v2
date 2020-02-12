dbfd7e29377a2459d344d9b1f766f0024077d26c
Takahiko Wada <wada.takahiko@gmail.com>
2020-02-12 14:04:11 +0900

    use FlatList

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, FlatList, SafeAreaView} from 'react-native';
 import ListItem from './components/ListItem';
 import articles from './dummies/articles';
 
@@ -7,20 +7,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
 export default function App() {
-  const items = articles.map((article, index) => (
-    <ListItem
-      imageUrl={article.urlToImage}
-      title={article.title}
-      author={article.author}
-      key={index}
-    />
-  ));
-
-  return <View style={styles.container}>{items}</View>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({item}) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
+  );
 }
