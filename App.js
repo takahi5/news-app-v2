fa907e4346f1747bf937e1caaba0060d5ed50484
Takahiko Wada <wada.takahiko@gmail.com>
2020-02-12 13:16:57 +0900

    Define style

--- App.js
+++ App.js
@@ -1,21 +1,6 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View
-        style={{
-          height: 100,
-          width: '100%',
-          borderColor: 'gray',
-          borderWidth: 1,
-        }}
-      />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -23,4 +8,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.itemContainer} />
+    </View>
+  );
+}
