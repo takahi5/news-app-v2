cfc81ec5602c0a267434f7717b53e4cf81edb62d
Takahiko Wada <wada.takahiko@gmail.com>
2020-02-12 14:54:40 +0900

    Added HomeScreen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, FlatList, SafeAreaView} from 'react-native';
+import ListItem from '../components/ListItem';
+import Constants from 'expo-constants';
+import axios from 'axios';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`;
+
+export default HomeScreen = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      console.log(response);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({item}) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
+  );
+};
