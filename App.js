e25785561a677886140feb543a826701fb7e306d
Takahiko Wada <wada.takahiko@gmail.com>
2020-02-12 15:00:33 +0900

    Hide header in HomeScreen

--- App.js
+++ App.js
@@ -9,7 +9,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
