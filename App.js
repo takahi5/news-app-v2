c98381ddec526631a74402b8a4098c4e1b0d37b7
Takahiko Wada <wada.takahiko@gmail.com>
2020-02-12 13:13:50 +0900

    Changed text

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello React Native</Text>
     </View>
   );
 }
