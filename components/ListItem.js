701c533959556b3dd10ed34261af27eb4a974ca6
Takahiko Wada <wada.takahiko@gmail.com>
2020-02-12 14:54:52 +0900

    Fixed error when image url is null

--- components/ListItem.js
+++ components/ListItem.js
@@ -30,7 +30,9 @@ const ListItem = ({imageUrl, title, author}) => {
   return (
     <View style={styles.itemContainer}>
       <View style={styles.leftContainer}>
-        <Image style={{width: 100, height: 100}} source={{uri: imageUrl}} />
+        {!!imageUrl && (
+          <Image style={{width: 100, height: 100}} source={{uri: imageUrl}} />
+        )}
       </View>
       <View style={styles.rightContainer}>
         <Text numberOfLines={3} style={styles.text}>
