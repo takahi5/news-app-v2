b5b5bedea51fca1eb536ce8e2a20b4aeebe42018
Takahiko Wada <wada.takahiko@gmail.com>
2020-02-12 13:19:20 +0900

    Layourt of leftContainer and lightContainer

--- App.js
+++ App.js
@@ -13,13 +13,25 @@ const styles = StyleSheet.create({
     width: '100%',
     borderColor: 'gray',
     borderWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    backgroundColor: 'red',
+    width: 100,
+  },
+  rightContainer: {
+    backgroundColor: 'blue',
+    flex: 1,
   },
 });
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.itemContainer} />
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}></View>
+        <View style={styles.rightContainer}></View>
+      </View>
     </View>
   );
 }
