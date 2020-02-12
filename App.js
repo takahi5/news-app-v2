a32b0557de637134a717ac1c646d39c1e29b8d32
Takahiko Wada <wada.takahiko@gmail.com>
2020-02-12 14:28:30 +0900

    Use API to fetch articles

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import {StyleSheet, FlatList, SafeAreaView} from 'react-native';
 import ListItem from './components/ListItem';
 import dummyArticles from './dummies/articles';
+import Constants from 'expo-constants';
+import axios from 'axios';
 
 const styles = StyleSheet.create({
   container: {
@@ -10,16 +12,24 @@ const styles = StyleSheet.create({
   },
 });
 
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`;
+
 export default function App() {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setArticles(dummyArticles);
-    }, 2000);
-
-    return () => clearTimeout(timer);
+    fetchArticles();
   }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      console.log(response);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
