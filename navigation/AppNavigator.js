e08a94a37e18fa1de02073a7f816e9394fe53086
Takahiko Wada <wada.takahiko@gmail.com>
2020-02-12 19:22:46 +0900

    Added TabNavigator

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+/* screens */
 import HomeScreen from '../screens/HomeScreen';
 import ArticleScreen from '../screens/ArticleScreen';
+import ClipScreen from '../screens/ClipScreen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen name="Article" component={ArticleScreen} />
+    </Stack.Navigator>
+  );
+};
+
+const ClipStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Clip" component={ClipScreen} />
+    </Stack.Navigator>
+  );
+};
 
 export default AppNavigaotor = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen name="Article" component={ArticleScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Clip" component={ClipStack} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
