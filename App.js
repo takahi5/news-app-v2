5e2e9deaa278f171b3b3c5fd736c32b5f7f37acf
Takahiko Wada <wada.takahiko@gmail.com>
2020-09-19 05:18:58 +0900

    Axiosを使ってAPIを実行する

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import {StyleSheet, View, FlatList} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, View, FlatList, SafeAreaView} from 'react-native';
+import axios from 'axios';
+import Constants from 'expo-constants';
 import ListItem from './components/ListItem';
-import articles from './dummies/articles';
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 export default function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <FlatList
         data={articles}
         renderItem={({item}) => (
@@ -17,7 +36,7 @@ export default function App() {
         )}
         keyExtractor={(item, index) => index.toString()}
       />
-    </View>
+    </SafeAreaView>
   );
 }
 
