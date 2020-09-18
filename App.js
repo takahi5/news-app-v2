d6a1a1d94f04b1880358b9ee3461aa3d92759f29
Takahiko Wada <wada.takahiko@gmail.com>
2020-09-19 04:35:25 +0900

    レイアウトの調整

--- App.js
+++ App.js
@@ -12,12 +12,12 @@ export default function App() {
           />
         </View>
         <View style={styles.rightContainer}>
-          <Text numberOfLines={3}>
+          <Text numberOfLines={3} style={styles.text}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation
           </Text>
-          <Text>ReactNews</Text>
+          <Text style={styles.subText}>ReactNews</Text>
         </View>
       </View>
     </View>
@@ -43,5 +43,14 @@ const styles = StyleSheet.create({
   },
   rightContainer: {
     flex: 1,
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: 'gray',
   },
 });
