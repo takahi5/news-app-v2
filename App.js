ae3a8b21ed637ae6e169eb2b9890dd9ddc5da5a8
Takahiko Wada <wada.takahiko@gmail.com>
2020-09-19 04:51:53 +0900

    props経由でデータを渡す

--- App.js
+++ App.js
@@ -5,7 +5,21 @@ import ListItem from './components/ListItem';
 export default function App() {
   return (
     <View style={styles.container}>
-      <ListItem />
+      <ListItem
+        imageUrl="https://picsum.photos/id/10/200/200"
+        title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
+        author="SampleNews"
+      />
+      <ListItem
+        imageUrl="https://picsum.photos/id/10/200/200"
+        title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
+        author="SampleNews"
+      />
+      <ListItem
+        imageUrl="https://picsum.photos/id/10/200/200"
+        title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
+        author="SampleNews"
+      />
     </View>
   );
 }
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import {StyleSheet, View, Image, Text} from 'react-native';
 
-const ListItem = () => {
+const ListItem = ({imageUrl, title, author}) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{width: 100, height: 100}}
-            source={{uri: 'https://picsum.photos/id/10/200/200'}}
-          />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={3} style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation
-          </Text>
-          <Text style={styles.subText}>ReactNews</Text>
-        </View>
+    <View style={styles.itemContainer}>
+      <View style={styles.leftContainer}>
+        <Image style={{width: 100, height: 100}} source={{uri: imageUrl}} />
+      </View>
+      <View style={styles.rightContainer}>
+        <Text numberOfLines={3} style={styles.text}>
+          {title}
+        </Text>
+        <Text style={styles.subText}>{author}</Text>
       </View>
     </View>
   );
@@ -27,12 +20,6 @@ const ListItem = () => {
 export default ListItem;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   itemContainer: {
     height: 100,
     width: '100%',
