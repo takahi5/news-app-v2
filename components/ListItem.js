eafc82ec01770faea7e90f73d996cc5a95030d08
Takahiko Wada <wada.takahiko@gmail.com>
2020-09-19 04:52:48 +0900

    レイアウトの修正

--- components/ListItem.js
+++ components/ListItem.js
@@ -3,22 +3,20 @@ import {StyleSheet, View, Image, Text} from 'react-native';
 
 const ListItem = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{width: 100, height: 100}}
-            source={{uri: 'https://picsum.photos/id/10/200/200'}}
-          />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={3} style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation
-          </Text>
-          <Text style={styles.subText}>ReactNews</Text>
-        </View>
+    <View style={styles.itemContainer}>
+      <View style={styles.leftContainer}>
+        <Image
+          style={{width: 100, height: 100}}
+          source={{uri: 'https://picsum.photos/id/10/200/200'}}
+        />
+      </View>
+      <View style={styles.rightContainer}>
+        <Text numberOfLines={3} style={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation
+        </Text>
+        <Text style={styles.subText}>ReactNews</Text>
       </View>
     </View>
   );
@@ -27,12 +25,6 @@ const ListItem = () => {
 export default ListItem;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   itemContainer: {
     height: 100,
     width: '100%',
