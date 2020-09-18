209fb4bec18e2af2f51bf4807a40047023aeecb6
Takahiko Wada <wada.takahiko@gmail.com>
2020-09-19 05:02:40 +0900

    FlatList

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, FlatList} from 'react-native';
 import ListItem from './components/ListItem';
 import articles from './dummies/articles';
 
 export default function App() {
-  const items = articles.map((article, index) => {
-    return (
-      <ListItem
-        imageUrl={article.urlToImage}
-        title={article.title}
-        author={article.author}
-        key={index.toString()}
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({item}) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
       />
-    );
-  });
-
-  return <View style={styles.container}>{items}</View>;
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
