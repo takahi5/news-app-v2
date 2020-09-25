9dc82bcea89015e6f75a818366caf0a829a54620
Takahiko Wada <wada.takahiko@gmail.com>
2020-09-26 05:38:42 +0900

    WebViewの追加

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {StyleSheet, SafeAreaView, Text} from 'react-native';
+import {WebView} from 'react-native-webview';
 
 const styles = StyleSheet.create({
   container: {
@@ -11,7 +12,7 @@ const styles = StyleSheet.create({
 export default ArticleScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>This is Article Screen</Text>
+      <WebView source={{uri: 'https://expo.io'}} />
     </SafeAreaView>
   );
 };
