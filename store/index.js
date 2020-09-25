81d71389ca9f1b135b90c176705dddd9b9f84c11
Takahiko Wada <wada.takahiko@gmail.com>
2020-09-26 06:25:55 +0900

    debugger

--- store/index.js
+++ store/index.js
@@ -1,10 +1,12 @@
 import {createStore, combineReducers} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
 import userReducer from './reducers/user';
 
 const rootReducer = combineReducers({
   user: userReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
