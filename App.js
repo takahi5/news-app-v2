38164ba9e4cd98db18e72a0e3af38f6c27b3ad3b
Takahiko Wada <wada.takahiko@gmail.com>
2020-09-26 05:48:22 +0900

    AppNavigator追加

--- App.js
+++ App.js
@@ -1,20 +1,6 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import HomeScreen from './screens/HomeScreen';
-
-const Stack = createStackNavigator();
+import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <AppNavigator />;
 }
